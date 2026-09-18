d029810f3507d6427922021d675815a98fe032e9
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2026-09-18 17:28:23 +0500

    Implement network namespace management and address configuration

--- cmd/cni/main.go
+++ cmd/cni/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Wasay1567/zero-copy/pkg/network"
+)
+
+func main() {
+	if err := network.ConfigureNetNS("/run/netns/testns"); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("network namespace configured successfully")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Wasay1567/zero-copy
 
 go 1.26.5
+
+require golang.org/x/sys v0.48.0
